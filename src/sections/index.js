6fd2985d1822dfc149e5afad6011b691ea4e06d4
Ajmal-Ali-k <ajmalali749@gmail.com>
2023-09-22 01:23:35 +0530

    initail commit

---
+++ src/sections/index.js
@@ -0,0 +1,10 @@
+import CustomerReview from "./CustomerReview";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import PopularProduct from "./PopularProduct";
+import Services from "./Services";
+import SubScribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import SpecialOffer from "./SpecialOffer"
+
+export {CustomerReview , Footer, Hero, PopularProduct,Services,SubScribe,SuperQuality,SpecialOffer}
