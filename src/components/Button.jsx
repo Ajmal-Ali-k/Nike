3e695cfbd2f0def341ac954771cd1036cdd46341
Ajmal-Ali-k <ajmalali749@gmail.com>
2023-09-23 00:55:42 +0530

    popular product done

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ function Button({label,iconURL}) {
   return (
     <button className='flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red'>
         {label}
-        <img src={iconURL} alt="arrow-icon" className='ml-2 rounded-full' />
+        {iconURL &&<img src={iconURL} alt="arrow-icon" className='ml-2 rounded-full' />}
     </button>
   )
 }
--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import Button from "../components/Button";
+import { shoe8 } from "../assets/images";
 
 const SuperQuality = () => {
   return (
-    <div>SuperQuality</div>
-  )
-}
+    <section
+      id="about-us"
+      className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container"
+    >
+      <div className="flex flex-1 flex-col ">
+        <h2 className="text-4xl font-palanquin capitalize lg:max-w-lg font-bold ">
+          We Provide You {""}
+          <span className="text-coral-red">Super</span>
+          <span className="text-coral-red"> Quality </span>
+          Shoes
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum harum
+          esse vitae temporibus? Alias odio, reprehenderit suscipit incidunt
+          blanditiis reiciendis aspernatur, ratione hic quaerat modi vero at,
+          officia aperiam fuga!
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text">
+          Lorem Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum
+          harum
+        </p>
+        <div className="mt-11">
+          <Button label="view Details" />
+        </div>
+      </div>
+      <div className="flex-1 flex justify-center items-center">
+        <img src={shoe8} alt="shoe8" width={570} height={522} className="object-contain" />
 
-export default SuperQuality
+      </div>
+    </section>
+  );
+};
+
+export default SuperQuality;
